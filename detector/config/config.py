a1dc0e778ea7ad4c8cb48b9c571400369232774d
hephaestus88 <103326480+hephaestus88@users.noreply.github.com>
2023-02-24 16:21:19 -0500

    edited config file

--- detector/config/config.py
+++ detector/config/config.py
@@ -4,7 +4,7 @@ from detectron2.config import CfgNode as CN
 def add_det_config(cfg, train_dataset_name, val_dataset_name, num_classes, device, output_dir):
     
     
-    cfg.MODEL.META_ARCHITECTURE = "DetGeneralizedRCNN"
+    cfg.MODEL.META_ARCHITECTURE = "GeneralizedRCNN"
     # dataset
     cfg.DATASETS.TRAIN = (train_dataset_name,)
     cfg.DATASETS.TEST = (val_dataset_name,)
@@ -17,11 +17,11 @@ def add_det_config(cfg, train_dataset_name, val_dataset_name, num_classes, devic
     cfg.SOLVER.BASE_LR = 0.001
     
     cfg.SOLVER.WARMUP_ITERS = 1000
-    cfg.SOLVER.MAX_ITER = 2000
+    cfg.SOLVER.MAX_ITER = 500
     cfg.SOLVER.STEPS = (1000, 1500)
     cfg.SOLVER.GAMMA = 0.5
     
-    cfg.TEST_EVAL_PERIOD = 200
+    #cfg.TEST_EVAL_PERIOD = 200
     cfg.MODEL.ROI_HEADS.NUM_CLASSES = num_classes + 1
 
  
@@ -30,14 +30,17 @@ def add_det_config(cfg, train_dataset_name, val_dataset_name, num_classes, devic
     
     cfg.OUTPUT_DIR = output_dir
 
+    #enable AMP trainer 
+    #cfg.SOLVER.AMP.ENABLED = True
+
 
     #data visualization in wandb
     cfg.USE_WANDB = True  # Comment this out if you don't want to use Wandb
     cfg.WANDB_PROJECT_NAME = "obj-det001"  # Wandb project name to log the run to
-    cfg.VIS_PERIOD = 300  # Plot training results each <> iterations (sends them to Wandb)
+    cfg.VIS_PERIOD = 100  # Plot training results each <> iterations (sends them to Wandb)
     # # images to plot per visualization run "group", i.e. for RPN/ROI plots how many examples to show; 3 nicely fits in Wandb
     cfg.VIS_IMS_PER_GROUP = 3
-    cfg.VIS_MAX_PREDS_PER_IM = 40  # Maximum number of bounding boxes per image in visualization
+    cfg.VIS_MAX_PREDS_PER_IM = 20  # Maximum number of bounding boxes per image in visualization
     cfg.VIS_TEST = True  # Visualize outputs during inference as well
     
     return cfg 
